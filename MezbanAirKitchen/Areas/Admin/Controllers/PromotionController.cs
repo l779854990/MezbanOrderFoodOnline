43d7a5720b9bbceb24867a01f893587c025a8c0b
Hieu.Nguyen <hieu.nguyen@bmbsoft.com.vn>
2020-03-03 18:26:17 +0700

    update ui + controller

---
+++ MezbanAirKitchen/Areas/Admin/Controllers/PromotionController.cs
@@ -0,0 +1,23 @@
+﻿using MezbanAirKitchen.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MezbanService.Interfaces;
+
+namespace MezbanAirKitchen.Areas.Admin.Controllers
+{
+    public class PromotionController : BaseController
+    {
+
+        public PromotionController(ILanguageService languageService, IAspNetUserService aspNetUserService) : base(languageService, aspNetUserService)
+        {
+        }
+        // GET: Admin/Promotion
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MezbanAirKitchen/Assets/backend/dist/js/user/manage.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+    var filterState = false;
+    $("#userTable").DataTable({
+        'ajax': '',
+        'columnDefs': [
+            {
+                'targets': 0,
+                'checkboxes': {
+                    'selectRow': true
+                }
+            }
+        ],
+        'select': {
+            'style': 'multi'
+        },
+        "searching": false,
+        'order': [[1, 'asc']],
+        "autoWidth": false
+    });
+    $(".filterState").on("click", function () {
+        filterState = !filterState;
+        if (filterState) {
+            $(".formFilter").toggle("slow");
+            $(".arrow-down").show();
+            $(".arrow-up").hide();
+        } else {
+            $(".formFilter").toggle("slow");
+            $(".arrow-up").show()
+            $(".arrow-down").hide();
+        }
+    })
+});
